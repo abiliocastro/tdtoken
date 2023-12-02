f07d4473bc5fef76e6291b6dae302e771dd2d563
Leandro Beserra <leandrobeserra11@gmail.com>
2023-12-02 18:01:13 -0300

    add dashboard page

--- frontend/src/Pages/Dashboard.jsx
+++ frontend/src/Pages/Dashboard.jsx
@@ -4,7 +4,8 @@ import HeaderMenu from '../Components/HeaderMenu';
 import MenuItem from '../Components/MenuItem';
 import PanelBalances from '../Components/PanelBalances';
 
-import { FaWallet, FaMoneyBillTransfer } from "react-icons/fa6";
+import { FaWallet } from "react-icons/fa6";
+import TransactionItem from '../Components/TransactionItem';
 
 function Main() {
     return (
@@ -18,10 +19,15 @@ function Main() {
                         <MenuItem icon={ FaWallet } color="#ffffff" text="Chave Pix"/>
                         <MenuItem icon={ FaWallet } color="#ffffff" text="Chave Pix"/>
                         <MenuItem icon={ FaWallet } color="#ffffff" text="Chave Pix"/>
-                        {/* <MenuItem icon={ FaMoneyBillTransfer } color="#339E33" text="Transferir"/>
-                        <MenuItem icon={ FaWallet } color="#339E33" text="Transferir"/>
-                        <MenuItem icon={ FaMoneyBillTransfer } color="#339E33" text="Transferir"/> */}
                     </div>
+                </div>
+                <div className='transactions_container'>
+                    <span className='title'>Últimas movimentações</span>
+                    <hr />
+                    <TransactionItem date='02/12/2023' value={15.98} sender="Escolinha RN ME"/>
+                    <TransactionItem date='02/12/2023' value={12.80} sender="Maria Tereza"/>
+                    <TransactionItem date='01/12/2023' value={-57.10} sender="Toninho Tornado"/>
+                    <TransactionItem date='01/12/2023' value={17.50} sender="Chico Mariola"/>
                 </div>
             </div>
         </div>
