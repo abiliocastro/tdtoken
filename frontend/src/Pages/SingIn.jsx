c17b4ef4395713d2e134d98d0132d956b7854597
Leandro Beserra <leandrobeserra11@gmail.com>
2023-12-02 19:20:28 -0300

    refactoring the chap page

--- frontend/src/Pages/SingIn.jsx
+++ frontend/src/Pages/SingIn.jsx
@@ -1,7 +1,29 @@
+import { useRef } from 'react'
+import api from '../Api.js'
+
 import HeaderMenu from '../Components/HeaderMenu';
 import logo from '../assets/logo.svg'
 
 function SingIn() {
+
+    const userField = useRef(null);
+    const passwordField = useRef(null);
+
+    async function authentication(){
+        const user = userField.current.value;
+        const password = passwordField.current.value;
+
+        if(user && password){
+            console.log(user)
+            console.log(password)
+            const response = await api.post('/login', {
+                "email": user,
+                "password": password
+            })
+            console.log(response)
+        }
+    }
+
     return (
         <div>
             <HeaderMenu text='Entrar' />
@@ -14,13 +36,13 @@ function SingIn() {
                 </div>
 
                 <div style={{'margin-top': '45px'}}>
-                    <input type="text" placeholder="Email/Usuário"/>
-                    <input type="password" placeholder="Senha"/>
+                    <input type="text" ref={userField} placeholder="Email/Usuário"/>
+                    <input type="password" ref={passwordField} placeholder="Senha"/>
                     <p className='forget_password_option'>
                         <a href="http://">Esqueceu a senha ?</a>
                     </p>
 
-                    <button style={{'margin-top': '68px'}}>Entrar</button>
+                    <button style={{'margin-top': '68px'}} onClick={authentication}>Entrar</button>
                 </div>
 
                 <div className='create_account_text'>
